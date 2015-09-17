bbbe21925bb5cc79c15263c9a54fa33eb82766ef
Linda Castillor <linda@raidho.mx>
2015-09-16 19:03:39 -0500

    Added stylesheet for slider and markup

---
+++ slider.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+Home Template
+Neue Raidho Website
+*/
+
+include "templates/header.php";
+
+?>
+
+	<section class="feat_banner gray_light_bg">
+
+		<div class="wrap">
+			<div class="image" style="background-image: url(img/feat_banner_back_a.jpg)"></div>
+			<div class="txt">
+				<a class="feat_project_slider_bttn" href="#">
+					<div class="feat_project_slider_info">
+						<h2><span class="red">Recent:</span> Brand and record art for The Guadaloops.</h2>
+						<p class="Decima">Check out our collaboration with these young and acclaimed Mexican musicians.</p>
+						<p class="Decima red">Read the full story →</p>
+					</div>
+				</a>
+			</div>
+		</div>
+
+	</section>
+
+
+
+<?php
+
+/*
+Extended Nav + Footer
+*/
+
+include "templates/footer.php";
+
+?>
