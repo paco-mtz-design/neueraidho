4f142c2de8f4e3b45efe7a2371add4a65e201641
Linda Castillor <linda@raidho.mx>
2015-09-16 19:05:35 -0500

    Added tabs on markup

--- slider.php
+++ slider.php
@@ -11,6 +11,13 @@ include "templates/header.php";
 
 	<section class="feat_banner gray_light_bg">
 
+		<ul class="slider-pager" id="slider3-pager">
+			<li><a href="#tab1"><p>New in store</p><h1>travel bags</h1></a></li>
+			<li><a href="#tab2"><p>kits</p><h1>Baby face shaving kit</h1></a></li>
+			<li><a href="#tab3"><p>new in store</p><h1>travel bags</h1></a></li>
+			<li><a href="#tab4"><p>new in store</p><h1>travel bags</h1></a></li>
+		</ul>
+		
 		<div class="wrap">
 			<div class="image" style="background-image: url(img/feat_banner_back_a.jpg)"></div>
 			<div class="txt">
