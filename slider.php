13aef1a6be1d55983bec6dd535824eda89980ed3
Linda Castillor <linda@raidho.mx>
2015-09-23 19:43:16 -0500

    Added styles on tabs

--- slider.php
+++ slider.php
@@ -9,20 +9,20 @@ include "templates/header.php";
 
 ?>
 
-	<section class="feat_banner gray_light_bg">
+	<section class="main_banner gray_light_bg">
 
 		<ul class="slider-pager" id="slider3-pager">
-			<li><a href="#tab1"><p>New in store</p><h1>travel bags</h1></a></li>
-			<li><a href="#tab2"><p>kits</p><h1>Baby face shaving kit</h1></a></li>
-			<li><a href="#tab3"><p>new in store</p><h1>travel bags</h1></a></li>
-			<li><a href="#tab4"><p>new in store</p><h1>travel bags</h1></a></li>
+			<li><a href="#tab1"><p>Simplicity</p></a></li>
+			<li><a href="#tab2"><p>Eloquency</p></a></li>
+			<li><a href="#tab3"><p>new in store</p></a></li>
+			<li><a href="#tab4"><p>new in store</p></a></li>
 		</ul>
 		
 		<div class="wrap">
 			<div class="image" style="background-image: url(img/feat_banner_back_a.jpg)"></div>
-			<div class="txt">
-				<a class="feat_project_slider_bttn" href="#">
-					<div class="feat_project_slider_info">
+			<div class="txt white_bg">
+				<a class="" href="#">
+					<div class="">
 						<h2><span class="red">Recent:</span> Brand and record art for The Guadaloops.</h2>
 						<p class="Decima">Check out our collaboration with these young and acclaimed Mexican musicians.</p>
 						<p class="Decima red">Read the full story →</p>
