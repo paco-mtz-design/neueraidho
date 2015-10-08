3f33a77472c05efabb145a946c0ad0d2f9b56b22
Martinez <martinez@raidho.mx>
2015-10-07 23:18:28 -0500

    Small fix to Top Nave menu elements spacing

--- templates/header.php
+++ templates/header.php
@@ -35,11 +35,11 @@ global $bclass;
 			<h3 id="nav_logo">RAIDHO</h3>
 
 			<ul id="nav" class="Decima">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="work.php">Work</a></li>
-				<li><a href="services.php">Services</a></li>
-				<li><a href="recent.php">Recent</a></li>
+				<li><a href="index.php">Home</a></li><li>
+				<a href="about.php">About</a></li><li>
+				<a href="work.php">Work</a></li><li>
+				<a href="services.php">Services</a></li><li>
+				<a href="recent.php">Recent</a></li>
 				<!-- <li><a href="contact.php">Contact</a></li> -->
 			</ul>
 			<ul id="dev_nav">
