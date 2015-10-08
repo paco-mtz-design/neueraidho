118bc27b9060e1918884b0fa3ce33a691a35bba1
Martinez <martinez@raidho.mx>
2015-10-07 20:44:27 -0500

    Single Blog template first stable version

--- blog-single.php
+++ blog-single.php
@@ -15,6 +15,51 @@ include "templates/header.php";
 		<img class="full_width_image" src="http://lorempixel.com/1280/740/technics">
 	</div>
 
+	<div class="wrap">
+		<div class="blog_wrap">
+			<div class="blog_header">
+				<h1 class="replica">Residente: El Potencial de Fundidora</h1>
+				<p class="regular_title Leitura">Colaboramos con la Revista Residente Monterrey para imaginar escenarios en los que el Parque Fundidora explote todo su potencial en un futuro cercano.</p>
+				<p class="meta Decima">Posted on Blog</br> 24/04/2014</p>
+				<ul class="share_btns replica">
+					<li><a href="#">Post to Facebook</a></li>
+					<li><a href="#">Post to Twitter</a></li>
+					<li><a href="#">Copy URL</a></li>
+				</ul>
+			</div>
+			<div class="blog_body Leitura">
+
+				<p>Hadas Steiner muestra la idea de la arquitectura como una condición en el entorno – donde el espacio se comprende mejor a través de la interacción que de la delineación. En el contexto regiomontano la acción patológica de circundar con muros cada espacio, públitco y privado, inhibe nuestra capacidad de interactuar con la ciudad. Por supuesto, la interacción con otras ciudadanos no está exenta de este padecimiento. Nuestro falso sentido de seguridad nos aliena.</p>
+
+				<img src="http://lorempixel.com/740/540/technics">
+
+				<p>Según Kevin Lynch, una ciudad legible es aquella en la cual sus distritos, hitos y caminos son fácil de identificar y agrupar dentro de un patrón absoluto. Tomando esto en cuenta, Fundidora padece de una serie de problemas que empobrecen la experiencia del parque. Independientemente del medio de transporte utilizado para llegar, el nivel de complejidad es absurdo, las entradas son poco identificables y el tráfico que se genera en los alrededores puede ser exhaustivo. Fundidora empieza y termina en rejas, no se integra, no permea en el entorno inmediato.</p>
+
+				<ul>
+					<li>Experiencia de usuario y comunicación</li>
+					<li>Interfaz, diseño de información y estrategia de contenido</li>
+					<li>Espacios mediatizados e Interacción</li>
+				</ul>
+			</div>
+
+			<div class="blog_foot">
+				<ul class="share_btns replica">
+					<li><a href="#">Post to Facebook</a></li>
+					<li><a href="#">Post to Twitter</a></li>
+					<li><a href="#">Copy URL</a></li>
+				</ul>
+
+				<div class="blog_more">
+				<h3 class="Decima">More Blog Posts</h3>
+					<ul>
+						<li><img src="http://lorempixel.com/600/600/technics" alt=""><a href="#">BPR Monterrey, 4th Edition</a></li>
+						<li><img src="http://lorempixel.com/600/600/technics" alt=""><a href="#">Finishing that custom type, and how hackers might have some advice on the subject.</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 
 
@@ -23,7 +68,7 @@ include "templates/header.php";
 /*
 Extended Nav + Footer
 */
-
+include "templates/extended_nav.php";
 include "templates/footer.php";
 
 ?>
