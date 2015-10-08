c06464ef123edb0fad79bdc130cfeed6948d0a46
Martinez <martinez@raidho.mx>
2015-10-07 22:55:42 -0500

    Log Single template first stable version

--- log-single.php
+++ log-single.php
@@ -5,11 +5,26 @@ Single Log Template
 Neue Raidho Website					:	10%
 */
 
-$bclass = "page";
 include "templates/header.php";
 ?>
 
 <div class="wrap">
+
+	<div class="log_single_wrap">
+		<div>
+			<img src="http://lorempixel.com/640/640/technics">
+		</div>
+		<div>
+			<h4>#Linda #Dinosaurios #Coder</h4>
+			<p>Posted on Instagram</br> 24/04/2014</p>
+			<ul class="share_btns squared_btns">
+				<li><a href="#">Post to Facebook</a></li>
+				<li><a href="#">Post to Twitter</a></li>
+				<li><a href="#">Pin this</a></li>
+				<li><a href="#">Copy URL</a></li>
+			</ul>
+		</div>
+	</div>
 
 </div>
 
