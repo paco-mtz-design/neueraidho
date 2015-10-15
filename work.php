a0efd7570f057398d5337dd38b1b1323c8e3a35a
Martinez <martinez@raidho.mx>
2015-10-14 21:04:07 -0500

    Se añade contenedor alternativo para Work que permite extender el ancho en vista Pantalla Grande

--- work.php
+++ work.php
@@ -12,8 +12,10 @@ include "templates/header.php";
 <section id="recent_work">
 
 	<div class="wrap">
-
 		<h2 class="Decima">Recent Work</h2>
+	</div>
+
+	<div class="wrap project_wrapper">
 
 		<div class="bl-party-two-w-captions">
 	        <ul>
