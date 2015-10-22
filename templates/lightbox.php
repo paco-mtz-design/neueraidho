82e09c6cad53fe01024c73dcc33a9ec2289dcbe8
Linda Castillor <linda@raidho.mx>
2015-10-21 20:30:02 -0500

    Split containers, added close button image

--- templates/lightbox.php
+++ templates/lightbox.php
@@ -1,54 +1,65 @@
-
 <section class="lightbox" id="1">
 	<div class="wrap">
 
-		<button class="prev_btn" onclick="$.fancybox.prev(true)"/>left</button>
+		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
 
-		<div class="image">
-			<img src="http://lorempixel.com/430/430/business" alt="yesyes" with="660" height="660">
+		<div class="image"> 
+			<img src="http://lorempixel.com/430/430/business">
 		</div>
 
 		<div class="content">
-			<div class="tags"></div>
-			<div class="info"></div>
-			<div class="social">
-				<ul>
-					<li><a href="#">Post to Facebook</a></li>
-					<li><a href="#">Post to Facebook</a></li>
-					<li><a href="#">Post to Facebook</a></li>
-					<li><a href="#">Post to Facebook</a></li>
-				</ul>
+			<div>
+				<div class="top">
+					<div class="tags">#Linda #Dinosaurios #Coder</div>
+					<div class="info">Posted on Instagram 24/04/2014</div>
+				</div>
+				<div class="bottom">
+					<div class="social">
+						<ul>
+							<li><a href="#">Post to Facebook</a></li>
+							<li><a href="#">Post to Facebook</a></li>
+							<li><a href="#">Post to Facebook</a></li>
+							<li><a href="#">Post to Facebook</a></li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 
-		<button class="close_btn" onclick="$.fancybox.close(true)" />close</button>
-		<button class="next_btn" onclick="$.fancybox.next(true)"/>right</button>
+		<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
+		<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
 	</div>
 </section>
 
 <section class="lightbox" id="1">
 	<div class="wrap">
 
-		<button class="prev_btn" onclick="$.fancybox.prev(true)"/>left</button>
+		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
 
 		<div class="image"> 
-			<img src="http://lorempixel.com/430/430/business" alt="yesyes" with="660" height="660">
+			<img src="http://lorempixel.com/430/430/business">
 		</div>
 
 		<div class="content">
-			<div class="tags"></div>
-			<div class="info"></div>
-			<div class="social">
-				<ul>
-					<li><a href="#">Post to Facebook</a></li>
-					<li><a href="#">Post to Facebook</a></li>
-					<li><a href="#">Post to Facebook</a></li>
-					<li><a href="#">Post to Facebook</a></li>
-				</ul>
+			<div>
+				<div class="top">
+					<div class="tags">#Linda #Dinosaurios #Coder</div>
+					<div class="info">Posted on Instagram 24/04/2014</div>
+				</div>
+				<div class="bottom">
+					<div class="social">
+						<ul>
+							<li><a href="#">Post to Facebook</a></li>
+							<li><a href="#">Post to Facebook</a></li>
+							<li><a href="#">Post to Facebook</a></li>
+							<li><a href="#">Post to Facebook</a></li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 
-		<button class="close_btn" onclick="$.fancybox.close(true)" />close</button>
-		<button class="next_btn" onclick="$.fancybox.next(true)"/>right</button>
+		<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
+		<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
 	</div>
 </section>
