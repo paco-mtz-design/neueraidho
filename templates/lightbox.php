6284ac6709bc899f8cb06ec2bf5fd527111214bf
Linda Castillor <linda@raidho.mx>
2015-10-21 19:57:52 -0500

    Added arrows, centering lightbox container

--- templates/lightbox.php
+++ templates/lightbox.php
@@ -1,20 +1,10 @@
-<?php
-
-/*
-Home Template
-Neue Raidho Website
-*/
-
-include "templates/header.php";
-
-?>
 
 <section class="lightbox" id="1">
 	<div class="wrap">
 
-		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
+		<button class="prev_btn" onclick="$.fancybox.prev(true)"/>left</button>
 
-		<div class="feature-image"> <!-- Instagram Image -->
+		<div class="image">
 			<img src="http://lorempixel.com/430/430/business" alt="yesyes" with="660" height="660">
 		</div>
 
@@ -31,17 +21,17 @@ include "templates/header.php";
 			</div>
 		</div>
 
-		<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
-		<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
+		<button class="close_btn" onclick="$.fancybox.close(true)" />close</button>
+		<button class="next_btn" onclick="$.fancybox.next(true)"/>right</button>
 	</div>
 </section>
 
 <section class="lightbox" id="1">
 	<div class="wrap">
 
-		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
+		<button class="prev_btn" onclick="$.fancybox.prev(true)"/>left</button>
 
-		<div class="feature-image"> <!-- Instagram Image -->
+		<div class="image"> 
 			<img src="http://lorempixel.com/430/430/business" alt="yesyes" with="660" height="660">
 		</div>
 
@@ -58,17 +48,7 @@ include "templates/header.php";
 			</div>
 		</div>
 
-		<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
-		<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
+		<button class="close_btn" onclick="$.fancybox.close(true)" />close</button>
+		<button class="next_btn" onclick="$.fancybox.next(true)"/>right</button>
 	</div>
 </section>
-
-<?php
-
-/*
-Extended Nav + Footer
-*/
-
-include "templates/footer.php";
-
-?>
---
+++ tester.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Home Template
+Neue Raidho Website
+*/
+
+include "templates/header.php";
+
+?>
+
+<a class="fancybox" href="#1" rel="first">fancy box</a>
+
+<?php include('templates/lightbox.php');?>
+
+<?php
+
+/*
+Extended Nav + Footer
+*/
+
+include "templates/footer.php";
+
+?>
