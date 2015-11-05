8eb0512ddc4b793211db2a4c283420d4467a01fd
Linda Castillor <linda@raidho.mx>
2015-11-04 19:00:21 -0600

    Add styles on breaks

--- templates/lightbox.php
+++ templates/lightbox.php
@@ -2,39 +2,40 @@
 	<div class="wrap">
 
 		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
-
-		<div class="image"> 
-			<img src="http://lorempixel.com/430/430/business">
-		</div>
-
-		<div class="content">
-			<div>
-				<div class="top">
-					<div class="tags">
-						<ul>
-							<li><a href="#">#Linda</a></li>
-							<li><a href="#">#Dinosaurios</a></li>
-							<li><a href="#">#Coder</a></li>
-						</ul>
-					</div>
-					<div class="info">Posted on Instagram 24/04/2014</div>
-				</div>
-				<div class="bottom">
-					<div class="social">
-						<ul>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<div class="right_controls">
 			<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
 			<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
+		</div>
+
+		<div class="container">
+			<div class="image"> 
+				<img src="http://lorempixel.com/430/430/business">
+			</div>
+
+			<div class="content">
+				<div>
+					<div class="top">
+						<div class="tags">
+							<ul>
+								<li><a href="#">#Linda</a></li>
+								<li><a href="#">#Dinosaurios</a></li>
+								<li><a href="#">#Coder</a></li>
+							</ul>
+						</div>
+						<div class="info">Posted on Instagram 24/04/2014</div>
+					</div>
+					<div class="bottom">
+						<div class="social">
+							<ul>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
@@ -43,39 +44,40 @@
 	<div class="wrap">
 
 		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
-
-		<div class="image"> 
-			<img src="http://lorempixel.com/430/430/business">
-		</div>
-
-		<div class="content">
-			<div>
-				<div class="top">
-					<div class="tags">
-						<ul>
-							<li><a href="#">#Linda2</a></li>
-							<li><a href="#">#Dinosaurios</a></li>
-							<li><a href="#">#Coder</a></li>
-						</ul>
-					</div>
-					<div class="info">Posted on Instagram 24/04/2014</div>
-				</div>
-				<div class="bottom">
-					<div class="social">
-						<ul>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<div class="right_controls">
 			<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
 			<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
+		</div>
+
+		<div class="container">
+			<div class="image"> 
+				<img src="http://lorempixel.com/430/430/business">
+			</div>
+
+			<div class="content">
+				<div>
+					<div class="top">
+						<div class="tags">
+							<ul>
+								<li><a href="#">#Linda</a></li>
+								<li><a href="#">#Dinosaurios</a></li>
+								<li><a href="#">#Coder</a></li>
+							</ul>
+						</div>
+						<div class="info">Posted on Instagram 24/04/2014</div>
+					</div>
+					<div class="bottom">
+						<div class="social">
+							<ul>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
@@ -84,39 +86,40 @@
 	<div class="wrap">
 
 		<button class="prev_btn" onclick="$.fancybox.prev(true)"/></button>
-
-		<div class="image"> 
-			<img src="http://lorempixel.com/430/430/business">
-		</div>
-
-		<div class="content">
-			<div>
-				<div class="top">
-					<div class="tags">
-						<ul>
-							<li><a href="#">#Linda3</a></li>
-							<li><a href="#">#Dinosaurios</a></li>
-							<li><a href="#">#Coder</a></li>
-						</ul>
-					</div>
-					<div class="info">Posted on Instagram 24/04/2014</div>
-				</div>
-				<div class="bottom">
-					<div class="social">
-						<ul>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-							<li><a href="#">Post to Facebook</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<div class="right_controls">
 			<button class="close_btn" onclick="$.fancybox.close(true)" /></button>
 			<button class="next_btn" onclick="$.fancybox.next(true)"/></button>
+		</div>
+
+		<div class="container">
+			<div class="image"> 
+				<img src="http://lorempixel.com/430/430/business">
+			</div>
+
+			<div class="content">
+				<div>
+					<div class="top">
+						<div class="tags">
+							<ul>
+								<li><a href="#">#Linda</a></li>
+								<li><a href="#">#Dinosaurios</a></li>
+								<li><a href="#">#Coder</a></li>
+							</ul>
+						</div>
+						<div class="info">Posted on Instagram 24/04/2014</div>
+					</div>
+					<div class="bottom">
+						<div class="social">
+							<ul>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+								<li><a href="#">Post to Facebook</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
