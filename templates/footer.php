4be5fc04a2ceab76facc137753440b2c753de1b5
pacster <martinez@raidho.mx>
2015-12-05 23:27:40 -0600

    Mejoras generales al footer:
    - Se pulen contenedores de ligas externas en vista teléfono
    - Se mejoran los esapciados
    - Se ocultan etiquetas de datos de contacto en vista teléfono

--- templates/footer.php
+++ templates/footer.php
@@ -16,19 +16,21 @@ Neue Raidho Website
 					64630
 				</li>
 				<li>
-					Mail: <a class="red" href="mailto:info@raidho.mx">info@raidho.mx</a><br>
-					Phone: <a class="red" href="">+52 81 8333 8920</a><br>
-					Skype: <span class="red">raidho.st</span><br>
+					<span class="phone_hide">Mail:</span> <a class="red" href="mailto:info@raidho.mx">info@raidho.mx</a><br>
+					<span class="phone_hide">Phone:</span> <a class="red" href="">+52 81 8333 8920</a><br>
+					<span class="phone_hide">Skype:</span> <span class="red">raidho.st</span><br>
 				</li>
 				<li>
-					<a href="#">Behance</a><br>
-					<a href="#">Facebook</a><br>
-					<a href="#">Instagram</a><br>
-					<a href="#">Dribbble</a><br>
-					<a href="#">Twitter</a>
+					<div>
+						<span><a href="#">Behance</a></span>
+						<span><a href="#">Facebook</a></span>
+						<span><a href="#">Instagram</a></span>
+						<span><a href="#">Dribbble</a></span>
+						<span><a href="#">Twitter</a></span>
+					</div>
 				</li>
 			</ul>
-			<p class="Leitura">All rights reserved ©Raidho 2015<br>
+			<p class="Leitura foot_notice">All rights reserved ©Raidho 2015<br>
 			See our Privacy Statement (<i><a href="#">ver el Aviso de Privacidad</a></i>).
 			</p>
 	</div>
