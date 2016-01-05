6982fad8f2b29213b649b0c0a45836acc276e6ca
Linda Castillor <linda@raidho.mx>
2016-01-04 19:12:15 -0600

    Agregar clase .hidden_element con jQuery para page de recent y work

--- templates/footer.php
+++ templates/footer.php
@@ -55,6 +55,11 @@ Neue Raidho Website
 			// ABOUT
 			$("#about > div, #about .people_founders > div, #about .people_team > div ").addClass("hidden_element");
 			$("#about > div:nth-child(1), #about .wrap .section_pad").removeClass("hidden_element");
+			// WORK
+			$("#recent_work > .project_wrapper .bl-party-three-w-captions ul > li").addClass("hidden_element");
+			$("#recent_work > .project_wrapper .bl-party-four-w-captions ul > li").addClass("hidden_element");
+			// RECENT
+			$("#loader ul.log > li:nth-child(-n+5)").removeClass("hidden_element");
 	</script>
 
 </body>
