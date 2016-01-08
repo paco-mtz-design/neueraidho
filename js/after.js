550b2bfc738ccac42278db58de5e86415081df3a
Linda Castillor <linda@raidho.mx>
2016-01-08 16:00:38 -0600

    Fixes en slider/tabs y ajustes en efectos del home

--- js/after.js
+++ js/after.js
@@ -2,33 +2,43 @@
 var $container = $('.masonry');
 // initialize Masonry after all images have loaded
 $container.imagesLoaded( function() {
-  $container.masonry({
-    columnWidth: '.masonry_column',
-    gutter: '.masonry_gutter',
-    itemSelector: '.masonry_item'
-    });
+	$container.masonry({
+	columnWidth: '.masonry_column',
+	gutter: '.masonry_gutter',
+	itemSelector: '.masonry_item'
+	});
 });
 
-// LAZY LOAD
+// Lazy Load
 $(document).scroll(function () {
-        var y = $(this).scrollTop();
-        var h = window.innerHeight;
-        $('.hidden_element').each(function () {
-        var t = $(this).offset().top - h;
-        if (y > t) {
-            $(this).animate({'opacity':'1', 'margin-top':'0px'},400);
-        }
-    });
+  var y = $(this).scrollTop();
+  var h = window.innerHeight;
+  $('.hidden_element').each(function () {
+	var t = $(this).offset().top - h;
+	if (y > t) {
+			$(this).animate({'opacity':'1', 'margin-top':'0px'},400);
+		}
+	});
 });
 
-// LAZY LOAD APPLIED
-// HOME
+// Lazy Load Applied
+// Home
 $("ul.log > li").addClass("hidden_element");
-// ABOUT
+// About
 $("#about > div, #about .people_founders > div, #about .people_team > div ").addClass("hidden_element");
 $("#about > div:nth-child(1), #about .wrap .section_pad").removeClass("hidden_element");
-// WORK
+// Work
 $("#recent_work > .project_wrapper .bl-party-three-w-captions ul > li").addClass("hidden_element");
 $("#recent_work > .project_wrapper .bl-party-four-w-captions ul > li").addClass("hidden_element");
-// RECENT
+// Recent
 $("#loader ul.log > li:nth-child(-n+5)").removeClass("hidden_element");
+
+
+// Delay Home Container Elements
+//quickly setting up shortcuts, personal preference
+var head, lA, lB, lC, lD, h1;
+header = $('.home_cover');
+lA = $('.home_cover h1');
+
+header.animate({opacity:1}, 400, function() {});
+lA.delay(500).animate({opacity:1}, 400, function() {});
